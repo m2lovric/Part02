7c8ca5338bdc73f0f406eb873ec2c5c2f19c42a6
m2lovric <m2lovric@gmail.com>
2020-04-20 13:36:42 +0200

    exercises 2.12 update

--- countries/src/App.js
+++ countries/src/App.js
@@ -2,6 +2,44 @@ import React, {useState, useEffect} from 'react';
 import './App.css'
 import axios from 'axios';
 
+const Countries = ({filterC}) => {
+  return(
+    <div>
+      {
+        filterC.length > 10 ? 
+        'Too many matches, specify another filter.' : 
+        filterC.map(el => {
+          return <p>{el.name}</p>
+        })
+      }
+    </div>
+  )
+}
+
+const Country = ({filterC}) => {
+  return(
+    <div>
+      {
+        filterC.length == 1 ? 
+        filterC.map(el => {
+          return(
+            <div>
+              <h2>{el.name}</h2>
+              <p>capital: {el.capital}</p>
+              <p>population: {el.population}</p>
+              <h3>languages</h3>
+              <ul>
+                {el.languages.map(el => <li>{el.name}</li>)}
+              </ul>
+              <img src={el.flag} alt="flag"/>
+            </div>
+          )
+        }) : ''
+      }
+    </div>
+  )
+}
+
 function App() {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
@@ -24,28 +62,9 @@ function App() {
     <div className="App">
       <div>find countries: <input type="search" onChange={handleSearch} value={search}/></div>
       {
-        filterC.length > 10 ? 
-        'Too many matches, specify another filter.' : 
-        filterC.map(el => {
-          return <p>{el.name}</p>
-        })
-      }
-      {
-        filterC.length == 1 ? 
-        filterC.map(el => {
-          return(
-            <div>
-              <h2>{el.name}</h2>
-              <p>capital: {el.capital}</p>
-              <p>population: {el.population}</p>
-              <h3>languages</h3>
-              <ul>
-                {el.languages.map(el => <li>{el.name}</li>)}
-              </ul>
-              <img src={el.flag} alt="flag"/>
-            </div>
-          )
-        }) : ''
+        filterC.length === 1 ?
+        <Country filterC={filterC} /> :
+        <Countries filterC={filterC} />
       }
     </div>
   );
