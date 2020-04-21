8c1c280a4f9bf010daea166d90925a6fd14fa659
m2lovric <m2lovric@gmail.com>
2020-04-21 13:26:34 +0200

    exercises 2.13 2.14

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,7 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import './App.css'
 import axios from 'axios';
-
+const ListCountry = ({el}) => {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () =>{
+    setToggle(!toggle);
+  }
+  return (
+    <div key={el.cioc}>
+      <p>{el.name}</p>
+      <button onClick={handleToggle}>show</button>
+      <div className={toggle ? 'active' : 'hidden'}>
+        <Country filterC={el}/>
+      </div>
+    </div>
+  )
+}
 const Countries = ({filterC}) => {
   return(
     <div>
@@ -9,33 +23,58 @@ const Countries = ({filterC}) => {
         filterC.length > 10 ? 
         'Too many matches, specify another filter.' : 
         filterC.map(el => {
-          return <p>{el.name}</p>
+          return <ListCountry el={el}/>
         })
       }
     </div>
   )
 }
 
-const Country = ({filterC}) => {
+const Country = ({filterC}) => {  
+  const [weather, setWeather] = useState({
+    temp: 0,
+    wind: 0,
+    icon:''
+  });
+  const data = filterC.capital;
+  console.log(data);
+  useEffect(() => {
+    const capital = data;
+    console.log(capital);
+    axios
+    .get(`http://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+    .then(res => {
+      setWeather(() => {
+        return({
+          temp: weather.temp + res.data.main.temp,
+          wind: weather.wind + res.data.wind.speed,
+          icon: weather.icon + res.data.weather[0].icon
+        });
+      });
+    })
+  }, [])
+  const arr = [filterC];
   return(
     <div>
       {
-        filterC.length == 1 ? 
-        filterC.map(el => {
+        arr.map(el => {
           return(
-            <div>
+            <div key={el.cioc}>
               <h2>{el.name}</h2>
               <p>capital: {el.capital}</p>
               <p>population: {el.population}</p>
               <h3>languages</h3>
               <ul>
-                {el.languages.map(el => <li>{el.name}</li>)}
+                {el.languages.map(el => <li key={el.name}>{el.name}</li>)}
               </ul>
-              <img src={el.flag} alt="flag"/>
+              <img src={el.flag} alt="flag"/>        
             </div>
           )
-        }) : ''
-      }
+        })
+      }    
+      <p>temp: {weather.temp}</p>
+      <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="icon"/>
+      <p>wind speed: {weather.wind}</p>
     </div>
   )
 }
@@ -51,7 +90,8 @@ function App() {
       .then(res => {
         setCountries(res.data);
       })
-  })
+
+  },[])
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
@@ -63,7 +103,7 @@ function App() {
       <div>find countries: <input type="search" onChange={handleSearch} value={search}/></div>
       {
         filterC.length === 1 ?
-        <Country filterC={filterC} /> :
+        <Country filterC={filterC[0]} /> :
         <Countries filterC={filterC} />
       }
     </div>
